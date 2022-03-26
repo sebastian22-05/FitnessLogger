31cf9a3b610972e4d53c4249a8781d0ff6ad006d
sebastian22-05 <robertch2001@gmail.com>
2022-03-26 13:14:14 +0000

    Exercises are saved to the database with a date

--- app/src/main/java/com/example/fitnesslogger/DBHelper.kt
+++ app/src/main/java/com/example/fitnesslogger/DBHelper.kt
@@ -1,4 +1,95 @@
 package com.example.fitnesslogger
 
-class DBHelper {
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.fitnesslogger.databinding.FragmentFirstBinding
+
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+    // below is the method for creating a database by a sqlite query
+    override fun onCreate(db: SQLiteDatabase) {
+        // below is a sqlite query, where column names
+        // along with their data types is given
+        val query = ("CREATE TABLE " + TABLE_NAME + " ("
+                + ID_COL + " INTEGER PRIMARY KEY, " +
+                EXERCISE_COL + " TEXT," +
+                DATE_COL + " TEXT" + ")")
+
+        // we are calling sqlite
+        // method for executing our query
+        db.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // this method is to check if table already exists
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    // This method is for adding data in our database
+    fun addExercise(exercise : String, date : String ){
+
+        // below we are creating
+        // a content values variable
+        val values = ContentValues()
+
+        // we are inserting our values
+        // in the form of key-value pair
+        values.put(EXERCISE_COL, exercise)
+        values.put(DATE_COL, date)
+
+        // here we are creating a
+        // writable variable of
+        // our database as we want to
+        // insert value in our database
+        val db = this.writableDatabase
+
+        // all values are inserted into database
+        db.insert(TABLE_NAME, null, values)
+
+        // at last we are
+        // closing our database
+        db.close()
+    }
+
+    // below method is to get
+    // all data from our database
+    fun getExercise(): Cursor? {
+
+        // here we are creating a readable
+        // variable of our database
+        // as we want to read value from it
+        val db = this.readableDatabase
+
+        // below code returns a cursor to
+        // read data from the database
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+
+    }
+
+    companion object{
+        // here we have defined variables for our database
+
+        // below is variable for database name
+        private val DATABASE_NAME = "WorkoutDB"
+
+        // below is the variable for database version
+        private val DATABASE_VERSION = 1
+
+        // below is the variable for table name
+        val TABLE_NAME = "Exercises"
+
+        // below is the variable for id column
+        val ID_COL = "id"
+
+        // below is the variable for name column
+        val EXERCISE_COL = "exercise"
+
+        // below is the variable for age column
+        val DATE_COL = "date"
+    }
 }
