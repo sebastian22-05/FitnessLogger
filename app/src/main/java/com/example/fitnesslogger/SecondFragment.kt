7237b43a9ba4e4ecc96a6817634fe407b0c08984
sebastian22-05 <robertch2001@gmail.com>
2022-03-27 19:32:16 +0100

    Second fragment displays data from db

--- app/src/main/java/com/example/fitnesslogger/SecondFragment.kt
+++ app/src/main/java/com/example/fitnesslogger/SecondFragment.kt
@@ -5,8 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_second.view.*
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.fitnesslogger.databinding.FragmentSecondBinding
+import kotlinx.android.synthetic.*
+import java.util.ArrayList
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -23,14 +28,28 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentSecondBinding.inflate(inflater, container, false)
+
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var exerciseModelArrayList: ArrayList<ExerciseModel>? = null
+        var exerciseAdapter: ExerciseAdapter? = null
+        var exercisesRV: RecyclerView? = null
+
+        val db = context?.let { it1 -> DBHelper(it1, null) }
+
+        exerciseModelArrayList = db!!.readExercises()
+        exerciseAdapter = activity?.let { ExerciseAdapter(exerciseModelArrayList, it) }
+        exercisesRV = view?.findViewById(R.id.exerciseList)
+
+        val linearLayoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        exercisesRV!!.layoutManager = linearLayoutManager
+
+        exercisesRV!!.adapter = exerciseAdapter
 
     }
 
