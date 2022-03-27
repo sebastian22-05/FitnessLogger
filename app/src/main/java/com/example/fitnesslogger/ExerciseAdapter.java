22300cd6b1b5309b90ccf7fa427d9fee585d5656
sebastian22-05 <robertch2001@gmail.com>
2022-03-27 17:35:12 +0100

    Data Class made + reading from db with the data class

---
+++ app/src/main/java/com/example/fitnesslogger/ExerciseAdapter.java
@@ -0,0 +1,2 @@
+package com.example.fitnesslogger;public class ExerciseAdapter {
+}
---
+++ app/src/main/java/com/example/fitnesslogger/ExerciseModel.kt
@@ -0,0 +1,9 @@
+package com.example.fitnesslogger
+
+class ExerciseModel     // constructor
+    (// creating getter and setter methods
+    var exercise: String, var date: String
+) {
+    var id = 0
+
+}
